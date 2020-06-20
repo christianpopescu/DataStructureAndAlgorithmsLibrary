c1fd9ee7d03ce243140d30240af02adf76e698f5
Christian Popescu <christian.popescu@outlook.com>
2020-06-20 23:46:11 +0200

    CircularLinkedListNode

--- src/CSharp/DSALSolution/DSALibrary/Collections/CircularLinkedListNode.cs
+++ src/CSharp/DSALSolution/DSALibrary/Collections/CircularLinkedListNode.cs
@@ -1,8 +1,24 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
-namespace DSALibrary
+namespace DSALibrary.Collections
 {
-    public class Class1
+    public class CircularLinkedListNode<T>
     {
+        protected LinkedListNode<CircularLinkedListNode<T>> ListNode;
+
+        public T Value { get; set;}
+
+        public CircularLinkedListNode(T value)
+        {
+            this.Value = value;
+            ListNode = new LinkedListNode<CircularLinkedListNode<T>>(this);
+        }
+
+        public CircularLinkedListNode<T> Next()
+        {
+            if (ListNode.List == null) return null;                         // No list available
+            if (ListNode.Next == null) return ListNode.List.First.Value;    // go to first 
+            return ListNode.Next.Value;                                     // "Normal" next
+        }
     }
 }
