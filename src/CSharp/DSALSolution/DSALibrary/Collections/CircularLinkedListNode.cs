6e9535a223e549372c9d4d9bda74bd36cd9b1c61
Christian Popescu <christian.popescu@outlook.com>
2020-06-20 23:49:05 +0200

    Finished CircularLinkedListNode

--- src/CSharp/DSALSolution/DSALibrary/Collections/CircularLinkedListNode.cs
+++ src/CSharp/DSALSolution/DSALibrary/Collections/CircularLinkedListNode.cs
@@ -17,8 +17,15 @@ namespace DSALibrary.Collections
         public CircularLinkedListNode<T> Next()
         {
             if (ListNode.List == null) return null;                         // No list available
-            if (ListNode.Next == null) return ListNode.List.First.Value;    // go to first 
+            if (ListNode.Next == null) return ListNode.List.First.Value;    // loop to first 
             return ListNode.Next.Value;                                     // "Normal" next
+        }
+
+        public CircularLinkedListNode<T> Previous()
+        {
+            if (ListNode.List == null) return null;                         // No list available
+            if (ListNode.Previous == null) return ListNode.List.Last.Value;    // loop to last 
+            return ListNode.Previous.Value;                                     // "Normal" next
         }
     }
 }
